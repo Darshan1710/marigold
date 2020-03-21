41649d0fbb354f95eb5c1c764c91ad9326964904
Harshada <harshada@destaglobal.com>
2020-03-21 11:07:31 +0530

    bug fixed

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set('Asia/Kolkata');
 |
 */
 //$config['base_url'] = 'http://localhost/marigold/';	
-$config['base_url'] = 'http://demo.alphacore.in/';	
+$config['base_url'] = 'https://demo.alphacore.in/';	
 //
 /*//
 |--------------------------------------------------------------------------
